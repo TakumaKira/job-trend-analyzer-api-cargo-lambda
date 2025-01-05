64d6a68b826e5689041f9aef8b18b7b2169e2f52
Takuma Kira <takuma@kirakiraworx.com>
2025-01-05 20:31:31 +0900

    [test]: Get AWS secret

--- src/db/connect.rs
+++ src/db/connect.rs
@@ -2,11 +2,16 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::env;
 use dotenvy::dotenv;
+use crate::aws_secrets_manager::get_secret::get_secret;
 
 pub async fn establish_connection() -> PgPool {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    
+
+    let aws_secret_name = env::var("AWS_DB_SECRETS_NAME").expect("AWS_SECRET_NAME must be set");
+    let secret = get_secret(&aws_secret_name, None, None).await.expect("Failed to get secret");
+    println!("secret: {}", secret);
+
     PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
