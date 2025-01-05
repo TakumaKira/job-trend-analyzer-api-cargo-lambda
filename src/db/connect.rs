a6410a7eb4289e3910aa191af819ac5936bfb4e2
Takuma Kira <takuma@kirakiraworx.com>
2025-01-05 21:03:30 +0900

    [refactor]: Use database url made up from secrets

--- src/db/connect.rs
+++ src/db/connect.rs
@@ -16,15 +16,17 @@ struct DbSecrets {
 
 pub async fn establish_connection() -> PgPool {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    println!("database_url: {}", database_url);
     let aws_secret_name = env::var("AWS_DB_SECRETS_NAME").expect("AWS_SECRET_NAME must be set");
-    let secrets: DbSecrets = serde_json::from_str(&get_secret(&aws_secret_name, None, None).await.expect("Failed to get secret"))
+
+    let secrets: DbSecrets = serde_json::from_str(
+        &get_secret(&aws_secret_name, None, None)
+        .await
+        .expect("Failed to get secret")
+    )
         .expect("Failed to parse secrets JSON");
     println!("secrets: {:?}", secrets);
 
-    let db_url = format!("postgres://{}:{}@{}:{}/{}", secrets.username, secrets.password, secrets.host, secrets.port, secrets.dbname);
-    println!("db_url: {}", db_url);
+    let database_url = format!("postgres://{}:{}@{}:{}/{}", secrets.username, secrets.password, secrets.host, secrets.port, secrets.dbname);
 
     PgPoolOptions::new()
         .max_connections(5)
