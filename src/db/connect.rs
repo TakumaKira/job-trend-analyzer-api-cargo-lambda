417ecc2cb206e7519d3868dc12a0b7ac35839417
Takuma Kira <takuma@kirakiraworx.com>
2025-01-05 20:50:57 +0900

    [test]: Build db_url

--- src/db/connect.rs
+++ src/db/connect.rs
@@ -3,14 +3,28 @@ use sqlx::PgPool;
 use std::env;
 use dotenvy::dotenv;
 use crate::aws_secrets_manager::get_secret::get_secret;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct DbSecrets {
+    username: String,
+    password: String,
+    host: String,
+    port: String,
+    dbname: String,
+}
 
 pub async fn establish_connection() -> PgPool {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
+    println!("database_url: {}", database_url);
     let aws_secret_name = env::var("AWS_DB_SECRETS_NAME").expect("AWS_SECRET_NAME must be set");
-    let secret = get_secret(&aws_secret_name, None, None).await.expect("Failed to get secret");
-    println!("secret: {}", secret);
+    let secrets: DbSecrets = serde_json::from_str(&get_secret(&aws_secret_name, None, None).await.expect("Failed to get secret"))
+        .expect("Failed to parse secrets JSON");
+    println!("secrets: {:?}", secrets);
+
+    let db_url = format!("postgres://{}:{}@{}:{}/{}", secrets.username, secrets.password, secrets.host, secrets.port, secrets.dbname);
+    println!("db_url: {}", db_url);
 
     PgPoolOptions::new()
         .max_connections(5)
