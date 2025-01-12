5a9c891e773cfcc435c72ed55d4748c78876eb46
Takuma Kira <takuma@kirakiraworx.com>
2025-01-13 04:37:19 +0900

    Revert "[fix]: Remove tracing_subscriber"
    This reverts commit 474c78905bdf8d550ad2aadd5df1fd460caf14c1.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use lambda_http::{run, service_fn, Body, Error, Request, Response};
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
+    tracing_subscriber::fmt::init();
+
     let pool = establish_connection().await;
     let targets = get_targets(&pool).await?;
 
